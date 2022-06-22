012190f21db0357922b1dd56b8a4c2f3040ca6b0
GamerBike39 <champidub@gmail.com>
2022-06-22 19:56:22 +0200

    ajout du text typer

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
 let loader = document.querySelector(".chargement");
-let dotLoader = document.querySelector("#dotLoader");
-const dot1 = document.querySelector("#dotContainer div:nth-child(1)");
-const dot2 = document.querySelector("#dotContainer div:nth-child(2)");
-const dot3 = document.querySelector("#dotContainer div:nth-child(3)");
+let dotLoader = document.querySelector(".dotContainer");
+const dot1 = document.querySelector(".dotContainer div:nth-child(1)");
+const dot2 = document.querySelector(".dotContainer div:nth-child(2)");
+const dot3 = document.querySelector(".dotContainer div:nth-child(3)");
 
 function delay() {
   setTimeout(() => {
@@ -13,11 +13,24 @@ function delay() {
   }, 2500);
   setTimeout(() => {
     dot3.classList.add("dotLoader");
-  }, 3500);
-  // setTimeout(() => {
-  //   dot1.classList.remove("dotLoader");
-  //   dot2.classList.remove("dotLoader");
-  //   dot3.classList.remove("dotLoader");
-  // }, 5000);
+  }, 3000);
 }
 delay();
+
+var app = document.getElementById("writter");
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+});
+
+typewriter
+
+  .typeString("Une production")
+  .pauseFor(500)
+  .deleteAll()
+  .typeString("Access Code School")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Présente")
+  .pauseFor(3000)
+  .start();
