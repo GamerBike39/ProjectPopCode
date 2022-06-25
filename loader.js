ec88b8cef0a0de70ac45d136cf24fc174bdabebb
GamerBike39 <champidub@gmail.com>
2022-06-25 20:12:51 +0200

    loaderv2 with popSvg

--- loader.js
+++ loader.js
@@ -1,4 +1,12 @@
 let app = document.getElementById("writter");
+let neonText = document.querySelector("#titleNeon");
+let containerLoader = document.querySelector(".containerLoader");
+let popcode = document.querySelectorAll(" .neonText2 span");
+let popcorn = document.querySelectorAll(".pop");
+let chargement = document.querySelector("#chargement");
+let popSvg = document.getElementById("popCorn");
+let popopa = document.querySelector(".opacity");
+
 let typewriter = new Typewriter(app, {
   loop: false,
   delay: 50,
@@ -7,25 +15,68 @@ let typewriter = new Typewriter(app, {
 
 typewriter
 
-  .typeString("Bienvenue")
-  .pauseFor(500)
-  .deleteAll()
-  .typeString("Dans")
-  .pauseFor(500)
-  .deleteAll()
-  .typeString("<strong>🍿PopCode🍿</strong>")
-  .pauseFor(500)
-  .deleteAll()
-  .typeString("Une production")
-  .pauseFor(500)
-  .deleteAll()
   .typeString("Access Code School Lons")
-  .pauseFor(500)
+  .pauseFor(200)
   .deleteAll()
-  .typeString("<strong>🍿PopCode🍿</strong>")
+  .typeString("Présente")
+  .pauseFor(200)
   .start();
 0;
 
 setTimeout(() => {
-  app.style.fontSize = "8rem";
-}, 11500);
+  app.style.display = "none";
+  containerLoader.style.flexDirection = "column-reverse";
+  neonText.classList.remove("displayN");
+  popopa.style.opacity = "1";
+  chargement.style.display = "none";
+}, 5000);
+
+setTimeout(() => {
+  {
+    popcode[0].style.opacity = "1";
+  }
+}, 5200);
+setTimeout(() => {
+  {
+    popcode[1].style.opacity = "1";
+  }
+}, 5400);
+setTimeout(() => {
+  {
+    popcode[2].style.opacity = "1";
+  }
+}, 5600);
+setTimeout(() => {
+  {
+    popcode[3].style.opacity = "1";
+  }
+}, 5800);
+setTimeout(() => {
+  {
+    popcode[4].style.opacity = "1";
+  }
+}, 6000);
+setTimeout(() => {
+  {
+    popcode[5].style.opacity = "1";
+  }
+}, 6200);
+setTimeout(() => {
+  {
+    popcode[6].style.opacity = "1";
+  }
+}, 6400);
+
+// two variable x and y random numbers
+let x = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+let y = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+
+setTimeout(() => {
+  {
+    for (let i = 0; i < popcorn.length; i++) {
+      popcorn[i].style.stroke = "white";
+      popcorn[i].style.strokeWidth = "3px";
+      popcorn[i].style.fill = "#F2DA8D";
+    }
+  }
+}, 500);
