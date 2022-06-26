b16edd9e28655d50c37a4b26a02ec92d5a7674ba
GamerBike39 <champidub@gmail.com>
2022-06-26 16:21:42 +0200

    changement lumière popcode

--- loader.js
+++ loader.js
@@ -5,7 +5,7 @@ let popcode = document.querySelectorAll(" .neonText2 span");
 let popcorn = document.querySelectorAll(".pop");
 let chargement = document.querySelector("#chargement");
 let popSvg = document.getElementById("popSvg");
-let popopa = document.querySelector(".opacity");
+console.log(popcorn);
 
 let typewriter = new Typewriter(app, {
   loop: false,
@@ -29,50 +29,52 @@ setTimeout(() => {
   neonText.classList.remove("displayNone");
   popSvg.classList.remove("displayNone");
   chargement.style.display = "none";
+  for (let i = 0; i < popcorn.length; i++) {
+    popcorn[i].classList.add("displayNone");
+  }
 }, 5000);
 
 setTimeout(() => {
   {
     popcode[0].style.opacity = "1";
   }
-}, 5200);
-setTimeout(() => {
-  {
-    popcode[1].style.opacity = "1";
-  }
 }, 5400);
 setTimeout(() => {
   {
-    popcode[2].style.opacity = "1";
+    popcode[1].style.opacity = "1";
+    for (let i = 0; i < popcorn.length; i++) {
+      popcorn[i].classList.remove("displayNone");
+    }
   }
 }, 5600);
 setTimeout(() => {
   {
-    popcode[3].style.opacity = "1";
+    popcode[2].style.opacity = "1";
   }
 }, 5800);
 setTimeout(() => {
   {
-    popcode[4].style.opacity = "1";
+    popcode[3].style.opacity = "1";
   }
 }, 6000);
 setTimeout(() => {
   {
-    popcode[5].style.opacity = "1";
+    popcode[4].style.opacity = "1";
   }
 }, 6200);
 setTimeout(() => {
   {
-    popcode[6].style.opacity = "1";
+    popcode[5].style.opacity = "1";
   }
 }, 6400);
+setTimeout(() => {
+  {
+    popcode[6].style.opacity = "1";
+  }
+}, 6600);
 
 // setTimeout(() => {
 //   {
-//     for (let i = 0; i < popcorn.length; i++) {
-//       popcorn[i].style.stroke = "white";
-//       popcorn[i].style.strokeWidth = "3px";
-//       popcorn[i].style.fill = "#F2DA8D";
-//     }
+//     popcorn[0].classList.add("displayNone");
 //   }
 // }, 500);
